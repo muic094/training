d11def861e2cc2b74c2ccdf11fa4223cf5d0fc55
Music <avd04494@gmail.com>
2017-11-12 22:01:52 +0300

    First.

---
+++ src/main/java/com/example/web/training/TrainingApplication.java
@@ -0,0 +1,12 @@
+package com.example.web.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingApplication.class, args);
+	}
+}
